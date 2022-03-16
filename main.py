bf3bd2fe70bacbc5cb6a571e754273c96f5dbd52
Deff <Volobuev.Dmitriy46@yandex.ru>
2022-03-16 15:19:07 +0300

    Появилась дата генерируемого гороскопа, изменились шрифты, правильное расположение даты и заголовков относительно фото и текста

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
+from datetime import datetime
 
 
-# ------------------------- знаки для URL -------------------------------- #
+# ------------------------- Данные знаков и дат -------------------------------- #
+# Для парсинга
 signs = ['oven', 'telets', 'bliznetsi', 'rac', 'lev', 'deva', 'vesy', 'scorpion', 'strelets', 'kozerog', 'vodoley', 'riby']
+# Для печати на картинке
 signs_ru = ['Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева', 'Весы', 'Скорпион', 'Стрелец', 'Козерог', 'Водолей', 'Рыбы']
 title = dict(zip(signs, signs_ru))
+# Для правильного падежа даты
+months = {1: 'января', 2: 'февраля', 3: "марта", 4: "апреля", 5: "мая", 6: "июня",
+        7: "июля", 8: "августа", 9: "сентября", 10: "октября", 11: "ноября", 12: "декабря"}
 
 # ---------------------- Получаем гороскоп на день ---------------------- #
 def horoscope(sign):
@@ -22,15 +28,25 @@ def gluing_photo(t, sign):
     # Открываем фон, готовим шрифты
     image = Image.open('stars.jpg')
     font = ImageFont.truetype('timesi.ttf', 95)
-    title_font = ImageFont.truetype('comic.ttf', 280)
+    title_font = ImageFont.truetype('ANTQUAI.TTF', 300)
     draw = ImageDraw.Draw(image)
 
-    # Наносим текст
-    x = (image.size[0] - title_font.getsize(title[sign])[0]) // 2
-    draw.text((x, 50), title[sign], font=title_font, fill='#FFF')
+    # Наносим название знака
+    w_title = title_font.getsize(title[sign])[0]
+    x_sign = (image.size[0] // 2 - w_title) // 2 
+    draw.text((x_sign, 50), title[sign], font=title_font, fill='#FFF')
+
+    # Наносим сегодняшнюю дату и линию
+    today = datetime.now()
+    day = f'{today.day} {months[today.month]}' 
+    x_date = w_title + x_sign * 2
+    draw.text((x_date, 50), day, font=title_font, fill='#FFF')
+    draw.line(((200, 450), (3500, 450)), fill='white', width=15)
+    
+    # Наносим текст гороскопа
     y = 500
     for line in textwrap.wrap(t, width=37):
-        draw.text((1750, y), line, font=font, fill='#FFF')
+        draw.text((1750, y), line, font=font, fill='#FFFFFF')
         y += font.getsize(line)[1]
 
     # Настраиваем и вставляем фото знака
@@ -39,10 +55,13 @@ def gluing_photo(t, sign):
     sign_image = sign_image.resize((int(w * ((y - 500) / h)), y - 500))
     image.paste(sign_image, (250, 510))
 
+    # Сохраняем гороскоп
     image.save(f'{sign}_horoscope.jpg')
     
 
+# ---------------------- Главная функция ---------------------- #
 def main():
+    # Склеиваем гороскоп для каждого знака
     for sign in signs:
         gluing_photo(horoscope(sign), sign)
 
